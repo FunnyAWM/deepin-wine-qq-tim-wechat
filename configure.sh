fc904e8ce3b81a83979692f18540b12bfb704880
FunnyAWM <1142978300@qq.com>
2021-08-29 10:26:25 +0000

    update configure.sh.

--- configure.sh
+++ configure.sh
@@ -3,7 +3,7 @@ cd ~
 wget https://dldir1.qq.com/weixin/Windows/WeChat_for_XP_SP3_To_Vista.exe
 WINEPREFIX=~/.deepinwine/Deepin-WeChat deepin-wine WeChat_for_XP_SP3_To_Vista.exe
 rm WeChat_for_XP_SP3_To_Vista.exe
-wget -P PCQQ2021.exe https://558d35a266bf7034f9ae07b4a3c3e400.dlied1.cdntips.net/dlied1.qq.com/qqweb/PCQQ/PCQQ_EXE/PCQQ2021.exe
+wget https://558d35a266bf7034f9ae07b4a3c3e400.dlied1.cdntips.net/dlied1.qq.com/qqweb/PCQQ/PCQQ_EXE/PCQQ2021.exe -O PCQQ2021.exe
 WINEPREFIX=~/.deepinwine/Deepin-QQ deepin-wine PCQQ2021.exe
 cp ~/deepin-wine-qq-wechat-install/wqy-microhei.ttc ~/.deepinwine/Deepin-QQ/drive_c/windows/Fonts
 cp ~/deepin-wine-qq-wechat-install/wqy-microhei.ttc ~/.deepinwine/Deepin-WeChat/drive_c/windows/Fonts
