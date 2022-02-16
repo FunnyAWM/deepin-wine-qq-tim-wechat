9c257a9243ee578f68679d78bf63e7ea7c54d362
FunnyAWM <1142978300@qq.com>
2022-02-16 06:46:13 +0000

    新增TIM安装支持。

---
+++ TIM.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+echo "deb https://packages.deepin.com/deepin stable main contrib non-free" | sudo tee /etc/apt/sources.list.d/deepin.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 425956BB3E31DF51
+sudo apt update
+sudo apt install deepin.com.qq.office deepin.com.wechat -y
+sudo rm /etc/apt/sources.list.d/deepin.list&&sudo apt update
